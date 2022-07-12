e2bed11966d4daafd6e2f8c37bcb0232ef757859
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-07-11 21:26:29 -0500

    avance

--- arreglos/main.js
+++ arreglos/main.js
@@ -37,14 +37,61 @@ console.clear();
 
 document.querySelector("#peliculas-form").addEventListener("submit",agregar);
 
-function agregar (evt){
+let peliculas = 0;
+function agregar(evt){
     // console.log(evt.target);
     // alert(1);
 
-    evt.preventDefault();
-    console.log(evt.target);
+    evt.preventDefault(); // Esto es lo mimo que hacer ...
+    const {nombre, anio, foto} = evt.target;
+    // console.log(evt.target);
+
+    /*
     let nombre = document.querySelector("#nombre").ariaValueMax;
     let anio = document.querySelector("#anio").ariaValueMax;
     let foto = document.querySelector("#foto").ariaValueMax;
+    */
 
+   let peli = {
+    titulo: nombre.value,
+    fecha: anio.value,
+    caratula: foto.value
+   }
+
+
+   peliculas.push(peli);
+   evt.target.reset();
+    //    document.querySelector("#peliculas-form").reset();
+    
+    alert(`Pelicula guardada: ${peliculas.length}`);
+
+
+}
+
+function dibujar () {
+    // Voy a recorrer el array peliculas
+    /* 
+    for(let i = 0; i<peliculas.length; i++){
+    }
+
+    for(let n in peliculas) {
+        console.log(peliculas[n]);
+        document.querySelector("#resultados").innerHTML += 
+        `<div class="cpñ-4">
+            <div class="card mb-4">
+                <img src="${peliculas[n].caratula}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${peliculas[n].titulo}</h5>
+                    <p class="card-text">${peliculas[n].fecha}></p>
+
+                </div>
+            </div>
+        
+        </div>`;
+    }
+    */
+
+   for(let n in peliculas) {
+    console.log(peliculas[n]);
+   }
 }
